8b2708782bec898debdef63b8b08bcffc5abe2f8
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-10-24 16:11:29 +0700

    login

--- api/user/login.php
+++ api/user/login.php
@@ -3,12 +3,12 @@
     include_once("../../middlewares/cors.php");
     // add global variables
     include_once("../../globalVariables/index.php");
-    // check loggin yet, if Yes, not let them create again.
+    // middlewares
     include_once("../../middlewares/deserializeUser.php");
     include_once("../../middlewares/userExisted.php");
-    //////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
     $data = json_decode(file_get_contents("php://input"));
     $user->username = $data->username;
     $user->password = $data->password;
-    $user->create_user();
+    $user->login($key, $accessTokenTlt, $refreshTokenTlt, $cookiePath, $domain);
 ?>
